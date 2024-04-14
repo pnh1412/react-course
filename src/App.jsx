fe1ea33ab0c1a899e15f11658cbd3199e51a3242
tonynguyen <tonyjsfreelancer@gmail.com>
2024-04-14 11:00:31 +0700

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
 
 // components
-import Button from './components/Button';
 import Props from './Props';
 import State from './State';
 import Components from './Components';
@@ -26,92 +26,41 @@ import RefHooks from './RefHooks';
 import PerformanceHook from './PerformanceHook';
 import Collapse from './challenge/Collapse';
 import HauCollapse from './challenge/HauCollapse';
+import JSX from './JSX';
 
-function LargeText() {
-  return (
-    <span>large text</span>
-  )
-}
-
-function SmallText() {
-  return (
-    <span>small text</span>
-  )
-}
 
 function App() {
   // variables
   const count = 15; // valid javascript 
-  const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    avatar: 'https://picsum.photos/200/300',
-  }
-  const expressionJSX = (
-    <div>
-      express in JSX variable
-    </div>
-  )
-
-  // JSX expressions must have one parent element
-  const parentElement = (
-    <>
-      <div>parent element</div>
-      <div>parent element</div>
-    </>
-  )
-
-  // normal function of javasript by not PascalCase
-  function renderButton() {
-    return (
-      <button>Click me</button>
-    )
-  }
-
+  
   // JSX
   return (
-    <article>
+    <article className='p-4'>
       <h1>My First Component</h1>
-      <ol>
-        <li>Components: UI Building Blocks</li>
-        <li>Defining a Component</li>
-        <li>Using a Component</li>
-      </ol>
-
-      <h3>Expression in JSX</h3>
-      <div>Count: {count} </div>
-      <div>Operator: {count > 10 ? <span>large</span> : <span>small</span>}</div>
-      <div>Operator with react component: {count > 10 ? <LargeText /> : <SmallText />}</div>
+      <ul className="flex">
+        <li className="mr-6">
+          <Link to="/introduction-jsx" className="text-blue-500 hover:text-blue-800">Introduction JSX</Link>
+        </li>
+        <li className="mr-6">
+          <Link to="/props" className="text-blue-500 hover:text-blue-800">Props</Link>
+        </li>
+        <li className="mr-6">
+          <Link to="/state" className="text-blue-500 hover:text-blue-800">State</Link>
+        </li>
+      </ul>
+      <br />
+      <hr />
+      <br />
+      <Routes>
+        <Route path='/introduction-jsx' element={<JSX />} />
+        <Route path='/props' element={<Props />} />
+        <Route path='/state' element={<State />} />
+      </Routes>  
 
       <br />
-      <div>{expressionJSX}</div>
-      <div>JSX with function {renderButton()}</div>
-      <div>JSX with function {renderButton()}</div>
-      <br />
-
-      <div>JSX expressions must have one parent element: {parentElement}</div>
-
-      <br />
-      <div>render JSX with element must be  closed</div>
-      <div />
-
-      <br />
-      <div>render JSX with react function component</div>
-      <Button />
-
-      <br />
-      <div>
-        render JSX with images:
-        The alt attribute provides alternative information for an image if a 
-        user for some reason cannot view it (because of slow connection, an 
-        error in the src attribute, or if the user uses a screen reader).
-      </div>
-      <img src={user.avatar} width={40} height={40} alt="person" />
+      <hr />
+      
    
-      <Props />
-
-      <State />
-
       <Components 
         count={count}
       />
---
+++ src/JSX.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import Button from './components/Button';
+
+function LargeText() {
+  return (
+    <span>large text</span>
+  )
+}
+
+function SmallText() {
+  return (
+    <span>small text</span>
+  )
+}
+
+function JSX() {
+  const count = 15; // valid javascript 
+  
+  const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    avatar: 'https://picsum.photos/200/300',
+  }
+  const expressionJSX = (
+    <div>
+      express in JSX variable
+    </div>
+  )
+
+  // JSX expressions must have one parent element
+  const parentElement = (
+    <>
+      <div>parent element</div>
+      <div>parent element</div>
+    </>
+  )
+
+  // normal function of javasript by not PascalCase
+  function renderButton() {
+    return (
+      <button>Click me</button>
+    )
+  }
+
+
+  return (
+    <div>
+      <h2>JSX</h2>
+      <ol>
+        <li>Components: UI Building Blocks</li>
+        <li>Defining a Component</li>
+        <li>Using a Component</li>
+      </ol>
+
+      <h3>Expression in JSX</h3>
+      <div>Count: {count} </div>
+      <div>Operator: {count > 10 ? <span>large</span> : <span>small</span>}</div>
+      <div>Operator with react component: {count > 10 ? <LargeText /> : <SmallText />}</div>
+
+      <br />
+      <div>{expressionJSX}</div>
+      <div>JSX with function {renderButton()}</div>
+      <div>JSX with function {renderButton()}</div>
+      <br />
+
+      <div>JSX expressions must have one parent element: {parentElement}</div>
+
+      <br />
+      <div>render JSX with element must be  closed</div>
+      <div />
+
+      <br />
+      <div>render JSX with react function component</div>
+      <Button />
+
+      <br />
+      <div>
+        render JSX with images:
+        The alt attribute provides alternative information for an image if a 
+        user for some reason cannot view it (because of slow connection, an 
+        error in the src attribute, or if the user uses a screen reader).
+      </div>
+      <img src={user.avatar} width={40} height={40} alt="person" />
+    </div>
+  )
+}
+
+export default JSX
